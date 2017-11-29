289bdba8998eea4547ae71bd1d85c5e926d59159
CharlyGourdon <32136517+CharlyGourdon@users.noreply.github.com>
2017-11-29 15:53:09 +0100

    Add files via upload
    Mise en place des classes :
    NetworkWatcherHistory qui est la classe mère des deux classes NetworkWatcherHistoryMozilla et NetworkWatcherHistoryChrome qui servent à récupérer l'historique de Mozilla et Chrome.
    NetworkWatcherTCP qui récupère tout les paquets envoyés à partir du port 53.

--- src/UASurveillanceEngine/NetworkWatcher.java
+++ src/UASurveillanceEngine/NetworkWatcher.java
@@ -1,21 +1,21 @@
 package UASurveillanceEngine;
 
+import java.io.IOException;
 
-/**
- * 
- */
-public class NetworkWatcher extends Watcher {
-
-	/**
-	 * Default constructor
-	 */
+public abstract class NetworkWatcher extends Watcher{
+	
 	public NetworkWatcher() {
+		this.start();
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	public abstract void run();
 
+	
+	public static void main(String args[]) throws IOException{
+		NetworkWatcherHistoryMozilla mozilla = new NetworkWatcherHistoryMozilla("/home/etudiant/.mozilla/firefox/ugm37j7z.default-1462882570889/places.sqlite");
+		NetworkWatcherHistoryChrome chrome = new NetworkWatcherHistoryChrome("/home/etudiant/.config/google-chrome/Default/History");
+		NetworkWatcherTCP tcp = new NetworkWatcherTCP();
+	}
+	
 }
